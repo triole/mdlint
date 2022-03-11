2854812e8873d7c2a1ec79e0d807b10dd3e991cc
olaf michaelis <o.mic@web.de>
2022-03-11 09:11:35 +0100

    Remove print statement

--- src/main.go
+++ src/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"reflect"
 )
 
 func main() {
 	parseArgs()
-
-	fmt.Printf("%q\n", reflect.ValueOf(map[string]string{}).Kind())
 
 	mdFileList := []string{CLI.Target}
 	if isFolder(CLI.Target) == true {
